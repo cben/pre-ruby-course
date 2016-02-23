30f2b2a4e56c5c5b35ec5dd52b6bebbf84a4ff15
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-02-24 01:51:11 +0200

    Solve.

--- string_ext.rb
+++ string_ext.rb
@@ -1,2 +1,16 @@
 class StringExt
+  def initialize(first, second)
+    # TODO: better names?
+    # TODO: behavior if given non-Strings?
+    @first, @second = first, second
+  end
+
+  def concat
+    return @first + @second
+  end
+
+  # sic.  https://english.stackexchange.com/questions/3640/is-substract-versus-subtract-a-proper-word
+  def substract
+    return @first.gsub(@second, "")
+  end
 end
